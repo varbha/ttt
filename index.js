4b7637dd54fef2198361ae7bd4e9a8de5de5d91a
varbha <varunb95@gmail.com>
2018-04-29 19:22:29 +0530

    Heroku commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express  = require('express');
 var app = express();
 const body_parser = require('body-parser');
 
-
+var port = process.env.PORT || 3000;
 
 app.use(body_parser.urlencoded( {extended : true, parameterLimit : 1 }));
 app.use(body_parser.json());
@@ -12,7 +12,7 @@ app.set('view-engine', 'ejs');
 require('./routes/index')(app);
 
 
-app.listen(3000, function(){
+app.listen(port, function(){
     console.log("Listening..");
 });
 
